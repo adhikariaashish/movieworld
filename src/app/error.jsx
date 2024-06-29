c9b7e2ab5b23ef2ccabf41ab64f0788ea145c640
Aashish Adhikari <axisinfinity69@gmail.com>
2024-06-29 15:49:17 +0545

    bug fix

--- src/app/error.jsx
+++ src/app/error.jsx
@@ -1,15 +1,19 @@
 'use client';
 
-import React from 'react'
-import { useEffect } from 'react';
-export default function error({error, reset}) {
+import React, { useEffect } from 'react';
+
+export default function Error({ error, reset }) {
     useEffect(() => {
         console.log(error);
     }, [error]);
-  return (
-    <div className='text-center mt-10'>
-      <h1>Something went wrong. Please try again later.</h1>
-      <button className='hover:text-amber-600' onClick={()=>reset()}>Try again</button>
-    </div>
-  )
+
+    return (
+        <div className='text-center mt-10'>
+            <h1>Something went wrong. Please try again later.</h1>
+            <button className='hover:text-amber-600' onClick={reset}>
+                Try again
+            </button>
+        </div>
+    );
 }
+
