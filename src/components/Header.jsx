0d3abeb3598f8271039329826942fa373fc0c0af
Aashish Adhikari <axisinfinity69@gmail.com>
2025-01-24 10:07:38 +0545

    tweaks

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,24 +3,35 @@ import MenuItem from './MenuItem';
 import { AiFillHome } from 'react-icons/ai';
 import { BsFillInfoCircleFill } from 'react-icons/bs';
 import DarkModeSwitch from './DarkModeSwitch';
+import { SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs';
 
 export default function Header() {
   return (
     <div className='flex justify-between items-center p-3 max-w-6xl mx-auto'>
-      <div className='flex gap-12'>
-        <MenuItem title='HOME' address='/' Icon={AiFillHome} />
-        <MenuItem title='ABOUT' address='/about' Icon={BsFillInfoCircleFill} />
-        <MenuItem title='SIGN UP' address='/SignIn' Icon={AiFillHome} />
-      </div>
       <div className='flex items-center gap-4'>
-        <DarkModeSwitch />
         <Link href={'/'} className='flex gap-1 items-center'>
           <span className='text-2xl font-bold bg-amber-500 py-1 px-2 rounded-lg'>
             MovieWorld
           </span>
           
         </Link>
+        <DarkModeSwitch />
+        
+      </div>
+      <div className='flex gap-12'>
+        <MenuItem title='HOME' address='/' Icon={AiFillHome} />
+        <MenuItem title='ABOUT' address='/about' Icon={BsFillInfoCircleFill} />
+        
+        <SignedIn>  
+          <UserButton />
+        </SignedIn>
+        <SignedOut>   
+<MenuItem title='SIGN IN' address='/sign-in' Icon={AiFillHome} />
+          {/* <SignInButton /> */}
+        </SignedOut>
       </div>
     </div>
   );
 }
+
+
